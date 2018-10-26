bd6b8126165e471079f3d97de533f5d61dce80f6
Allan Tran <atran@inverse.ca>
2018-10-26 14:37:14 -0400

    Move JS into sslinspection.js instead of common.html

--- html/captive-portal/content/sslinspection.js
+++ html/captive-portal/content/sslinspection.js
@@ -1,4 +1,18 @@
 /* -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+$('#test').on('click', function() {
+  $.ajax({ 
+    url: 'https://packetfence.org/ssl-test/', 
+    method: 'GET' 
+  })
+ .done(function() {
+    window.location.href = "/captive-portal?next=next";
+ })
+ .fail(function() {
+    testFailed();
+ });
+ return false;
+});
+
 
 $(function() {
   'use strict';
