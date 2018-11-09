30281bc0eeffc65d4e25e1e21c9f3c3a5c86e5d8
Durand Fabrice <oeufdure@gmail.com>
2018-11-08 21:54:35 -0500

    Simplify the code and use the freeradius http headers to define the scope

--- lib/pf/api.pm
+++ lib/pf/api.pm
@@ -1291,7 +1291,7 @@ sub metadefender_process : Public {
 }
 
 sub rest_ping :Public :RestPath(/rest/ping){
-    my ($class, $args) = @_;
+    my ($class, $args, $headers) = @_;
     return "pong - ".$args->{message};
 }
 
@@ -1302,7 +1302,7 @@ RADIUS authorize method that uses REST
 =cut
 
 sub radius_rest_authorize :Public :RestPath(/radius/rest/authorize) {
-    my ($class, $radius_request) = @_;
+    my ($class, $radius_request, $headers) = @_;
     my $timer = pf::StatsD::Timer->new();
     my $logger = pf::log::get_logger();
 
@@ -1322,14 +1322,14 @@ sub radius_rest_authorize :Public :RestPath(/radius/rest/authorize) {
     return $return;
 }
 
-=head2 radius_rest_pre_proxy
+=head2 radius_rest_filter
 
-RADIUS pre-proxy method that uses REST
+RADIUS filter method that uses REST
 
 =cut
 
-sub radius_rest_pre_proxy :Public :RestPath(/radius/rest/pre_proxy) {
-    my ($class, $radius_request) = @_;
+sub radius_rest_filter :Public :RestPath(/radius/rest/filter) {
+    my ($class, $radius_request, $headers) = @_;
     my $timer = pf::StatsD::Timer->new();
     my $logger = pf::log::get_logger();
 
@@ -1337,60 +1337,13 @@ sub radius_rest_pre_proxy :Public :RestPath(/radius/rest/pre_proxy) {
 
     my $return;
 
-    $return = $class->radius_filter("preproxy",%remapped_radius_request);
+    $return = $class->radius_filter($headers->{'X-FreeRADIUS-Server'}.".".$headers->{'X-FreeRADIUS-Section'},%remapped_radius_request);
 
     # This will die with the proper code if it is a deny
     $return = pf::radius::rest::format_response($return);
 
     return $return;
 }
-
-=head2 radius_rest_post_proxy
-
-RADIUS post-proxy method that uses REST
-
-=cut
-
-sub radius_rest_post_proxy :Public :RestPath(/radius/rest/post_proxy) {
-    my ($class, $radius_request) = @_;
-    my $timer = pf::StatsD::Timer->new();
-    my $logger = pf::log::get_logger();
-
-    my %remapped_radius_request = %{pf::radius::rest::format_request($radius_request)};
-
-    my $return;
-
-    $return = $class->radius_filter("postproxy",%remapped_radius_request);
-
-    # This will die with the proper code if it is a deny
-    $return = pf::radius::rest::format_response($return);
-
-    return $return;
-}
-
-=head2 radius_rest_authorization
-
-RADIUS authorization method that uses REST
-
-=cut
-
-sub radius_rest_authorization :Public :RestPath(/radius/rest/authorization_filter) {
-    my ($class, $radius_request) = @_;
-    my $timer = pf::StatsD::Timer->new();
-    my $logger = pf::log::get_logger();
-
-    my %remapped_radius_request = %{pf::radius::rest::format_request($radius_request)};
-
-    my $return;
-
-    $return = $class->radius_filter("authorization",%remapped_radius_request);
-
-    # This will die with the proper code if it is a deny
-    $return = pf::radius::rest::format_response($return);
-
-    return $return;
-}
-
 
 =head2 radius_rest_switch_authorize
 
@@ -1399,7 +1352,7 @@ RADIUS switch authorize method that uses REST
 =cut
 
 sub radius_rest_switch_authorize :Public :RestPath(/radius/rest/switch/authorize) {
-    my ($class, $radius_request) = @_;
+    my ($class, $radius_request, $headers) = @_;
     my $timer = pf::StatsD::Timer->new();
     my $logger = pf::log::get_logger();
 
@@ -1420,7 +1373,7 @@ RADIUS accounting method that uses REST
 =cut
 
 sub radius_rest_accounting :Public :RestPath(/radius/rest/accounting) {
-    my ($class, $radius_request) = @_;
+    my ($class, $radius_request, $headers) = @_;
     my $timer = pf::StatsD::Timer->new();
     my $logger = pf::log::get_logger();
 
@@ -1430,7 +1383,7 @@ sub radius_rest_accounting :Public :RestPath(/radius/rest/accounting) {
 
     my $radius = new pf::radius::custom();
     eval {
-        $return = $radius->accounting(\%remapped_radius_request);
+        $return = $radius->accounting(\%remapped_radius_request, $headers);
     };
     if ($@) {
         $logger->error("radius accounting failed with error: $@");
